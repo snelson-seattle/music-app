3e0bc40f001b0b341e17d941ac02eb6cd7e892b9
Scott Nelson <snelson@instructors.2u.com>
2022-10-27 21:11:06 -0700

    successfully fetching WorldCharts from the shazam api using server side data fetching in the Home Page component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import { use } from "react";
+import api from "../api/shazamApi";
+
+const getWorldCharts = async () => {
+  const response = await api.get("/charts/world");
+  return response.data;
+};
+
 const Home = () => {
-  return (
-    <div>App goes here.</div>
-  )
-}
+  let songs = use(getWorldCharts());
+  console.log(songs);
+  return <div>App goes here.</div>;
+};
 
 export default Home;
